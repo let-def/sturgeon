c6c4b7c0704853b8c847abd3bb09aceddeabd852
Frederic Bour <frederic.bour@lakaban.net>
2016-07-13 16:02:33 +0900

    Add toplevel initialization script

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -8,6 +8,7 @@ let () =
   Ok [ Pkg.mllib "src/sturgeon.mllib"
      ; Pkg.mllib "src/sturgeon_recipes_command.mllib"
      ; Pkg.mllib "src/sturgeon_recipes_server.mllib"
+     ; Pkg.lib "sturgeon.top"
      ; Pkg.bin ~dst:"sturgeon-connector" "src/sturgeon_connector"
      ; Pkg.share_root ~dst:"emacs/site-lisp/sturgeon.el" "emacs/sturgeon.el"
      ]
